4027e4d1170adbc59d530863cee64e1c82ab2a4f
sadiabushra <sadiahossain3333@gmail.com>
2022-11-28 13:20:54 +0600

    lab task

---
+++ JS/JS.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <title> Login</title>
+        <link rel="stylesheet" href="JS.css">
+</head>
+<body>
+    <div class ="main-content">
+    <form action="action_page.php" method="post">
+  </div>
+
+  <div class="container">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required> <br>
+
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+
+    <button type="submit">Login</button>
+    <label>
+     <br> <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+  </div>
+
+  <div class="container" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn">Cancel</button>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
+</form>
+</body>
+</html>
